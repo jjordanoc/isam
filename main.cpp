d923b7dc33675417373c6d83aa25abe4df8e942c
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-04-19 12:45:48 -0500

    fix: read only entire blocks

--- main.cpp
+++ main.cpp
@@ -6,113 +6,43 @@ using namespace std;
 
 #define PAGE_SIZE 512
 
+// PAGE_SIZE = sizeof(KeyType) * M + sizeof(long) * (M+1) + sizeof(int)
 template<typename KeyType>
 const long M = (PAGE_SIZE - sizeof(int) - sizeof(long)) / (sizeof(KeyType) + sizeof(long));
 
+#define M M<KeyType>
+
 template<typename KeyType>
 struct IndexPage {
-    // PAGE_SIZE = sizeof(KeyType) * M + sizeof(long) * (M+1) + sizeof(int)
-
-    KeyType *keys = nullptr;
-    long *children = nullptr;
     int size = 0;
+    KeyType keys[M];
+    long children[M+1];
 
-    IndexPage() {
-        keys = new KeyType[M<KeyType>];
-        children = new long[M<KeyType> + 1];
-    }
-
-    void read(fstream &file) {
-        int i = 0, j = 0;
-        KeyType tmpKey;
-        long tmpChild;
-        file.read((char *) &tmpChild, sizeof(long));
-        children[j++] = tmpChild;
-        while (size < M<KeyType> && !file.eof()) {
-            file.read((char *) &tmpKey, sizeof(KeyType));
-            file.read((char *) &tmpChild, sizeof(long));
-            if (!file.eof()) {
-                keys[i++] = tmpKey;
-                children[j++] = tmpChild;
-                ++size;
-            }
-
-        }
-    }
+    IndexPage() = default;
 
     long locate(KeyType key) {
         int i = 0;
-        while (i < size && key <= keys[i]) {
+        while (i < size && key > keys[i]) {
            ++i;
         }
         return children[i];
     }
-
-    vector<long> locateTill(KeyType begin_key, KeyType end_key){
-        int beginPos;
-        int endPos;
-        vector<long> result;                   // ---From here
-        for(int i = 0; i < sizeof(keys); ++i){ // P0 K1 P1
-
-            if(begin_key <= keys[i]){
-                //begin_key == keys[i] ? beginPos = i : beginPos = i-1;
-                beginPos = i-1;
-                break;
-            }
-        }
-        beginPos++;
-        for(int i = beginPos; i < sizeof(keys); ++i){
-            if(end_key <= keys[i]){
-                //end_key == keys[i] ? endPos = i : endPos = i-1;
-                endPos = i-1;
-                break;
-            }
-        }
-        endPos++;
-
-        //get positions
-        for(int i = beginPos; i < endPos+1; ++i){
-            result.push_back(children[i]);
-        }
-
-        return result;
-    }
 };
 
+// PAGE_SIZE = sizeof(RecordType) * N + sizeof(int) + sizeof(long)
 template<typename RecordType>
 const long N = (PAGE_SIZE - sizeof(int) - sizeof(long)) / sizeof(RecordType);
 
+#define N N<RecordType>
+
 template<typename RecordType, typename KeyType>
 struct DataPage {
-    // PAGE_SIZE = sizeof(RecordType) * N + sizeof(int) + sizeof(long)
-    RecordType *records = nullptr;
+
     int size = 0;
+    RecordType records[N];
     long next = -1;
 
-    DataPage() {
-        records = new RecordType[N<RecordType>];
-    }
-
-    void read(fstream &file) {
-        int i = 0;
-        RecordType tmpRecord;
-        file.read((char *) &tmpRecord, sizeof(RecordType));
-        while (size < N<RecordType> && !file.eof()) {
-            if (!file.eof()) {
-                records[i++] = tmpRecord;
-                ++size;
-            }
-            file.read((char *) &tmpRecord, sizeof(RecordType));
-        }
-        file.read((char *) &next, sizeof(long));
-    }
-
-    void write(fstream &file) {
-        for (int i = 0; i < size; ++i) {
-            file.write((char *) &records[i], sizeof(RecordType));
-        }
-        file.write((char *) &next, sizeof(long));
-    }
+    DataPage() = default;
 
     vector<RecordType> locate(fstream &file, KeyType key) {
         vector<RecordType> result;
@@ -144,7 +74,7 @@ struct DataPage {
         return result;
     }
 
-    vector<RecordType> locateTill(fstream &file, KeyType begin_key, KeyType end_key){
+    vector<RecordType> locateRange(fstream &file, KeyType begin_key, KeyType end_key){
         vector<RecordType> result;
 
         int i = 0;
@@ -197,7 +127,7 @@ public:
         // read index file 1
         index1.open(indexfilename(1), flags);
         IndexPage<KeyType> indexPage1;
-        indexPage1.read(index1);
+        index1.read((char *) &indexPage1, PAGE_SIZE);
         long page1 = indexPage1.locate(index1, key);
         index1.close();
 
@@ -205,7 +135,7 @@ public:
         index2.open(indexfilename(2), flags);
         index2.seekg(page1);
         IndexPage<KeyType> indexPage2;
-        indexPage2.read(index2);
+        index2.read((char *) &indexPage2, PAGE_SIZE);
         long page2 = indexPage2.locate(index2, key);
         index2.close();
 
@@ -213,25 +143,25 @@ public:
         index3.open(indexfilename(3), flags);
         index3.seekg(page2);
         IndexPage<KeyType> indexPage3;
-        indexPage3.read(index3);
+        index3.read((char *) &indexPage3, PAGE_SIZE);
         long page3 = indexPage3.locate(index3, key);
         index3.close();
 
         file.open(filename, flags);
         file.seekg(page3);
         DataPage<RecordType, KeyType> dataPage;
-        dataPage.read(file);
-        if (dataPage.size == N<RecordType>) {
+        file.read((char *) &dataPage, PAGE_SIZE);
+        if (dataPage.size == N) {
             dataPage.next = file.tellg();
             file.seekp(dataPage.next);
             dataPage = DataPage<RecordType, KeyType>();
             dataPage.records[0] = record;
             dataPage.size = 1;
-            dataPage.write(file);
+            file.write((char *) &dataPage, PAGE_SIZE);
         } else {
             dataPage.records[dataPage.size++] = record;
             file.seekp(page3);
-            dataPage.write(file);
+            file.write((char *) &dataPage, PAGE_SIZE);
         }
         file.close();
     }
@@ -239,7 +169,7 @@ public:
         // read index file 1
         index1.open(indexfilename(1), flags);
         IndexPage<KeyType> indexPage1;
-        indexPage1.read(index1);
+        index1.read((char *) &indexPage1, PAGE_SIZE);
         long page1 = indexPage1.locate(key);
         index1.close();
 
@@ -247,7 +177,7 @@ public:
         index2.open(indexfilename(2), flags);
         index2.seekg(page1);
         IndexPage<KeyType> indexPage2;
-        indexPage2.read(index2);
+        index2.read((char *) &indexPage2, PAGE_SIZE);
         long page2 = indexPage2.locate(key);
         index2.close();
 
@@ -255,14 +185,14 @@ public:
         index3.open(indexfilename(3), flags);
         index3.seekg(page2);
         IndexPage<KeyType> indexPage3;
-        indexPage3.read(index3);
+        index3.read((char *) &indexPage3, PAGE_SIZE);
         long page3 = indexPage3.locate(key);
         index3.close();
 
         file.open(filename, flags);
         file.seekg(page3);
         DataPage<RecordType, KeyType> dataPage;
-        dataPage.read(file);
+        file.read((char *) &dataPage, PAGE_SIZE);
         vector<RecordType> records = dataPage.locate(file, key);
         file.close();
         return records;
@@ -273,35 +203,33 @@ public:
         // read index file 1
         index1.open(indexfilename(1), flags);
         IndexPage<KeyType> indexPage1;
-        indexPage1.read(index1);
-        long page1 = indexPage1.locate(index1, begin_key);
+        index1.read((char *) &indexPage1, PAGE_SIZE);
+        long page1 = indexPage1.locate(begin_key);
         index1.close();
 
         // read index file 2
         index2.open(indexfilename(2), flags);
         index2.seekg(page1);
         IndexPage<KeyType> indexPage2;
-        indexPage2.read(index2);
-        long page2 = indexPage2.locate(index2, begin_key);
+        index2.read((char *) &indexPage2, PAGE_SIZE);
+        long page2 = indexPage2.locate(begin_key);
         index2.close();
 
         // read index file 3
         index3.open(indexfilename(3), flags);
         index3.seekg(page2);
         IndexPage<KeyType> indexPage3;
-        indexPage3.read(index3);
-        long page3 = indexPage3.locate(index3, begin_key);
+        index3.read((char *) &indexPage3, PAGE_SIZE);
+        long page3 = indexPage3.locate(begin_key);
         index3.close();
 
         file.open(filename, flags);
         file.seekg(page3);
         DataPage<RecordType, KeyType> dataPage;
-        dataPage.read(file);
-        vector<RecordType> records = dataPage.locateTill(file, begin_key,end_key);
+        file.read((char *) &dataPage, PAGE_SIZE);
+        vector<RecordType> records = dataPage.locateRange(file, begin_key, end_key);
         file.close();
         return records;
-
-
     }
 };
 
@@ -329,7 +257,13 @@ public:
  * registro = localizar(dataPage, searchKey)
  */
 
+struct Alumno {
+    char codigo[10];
+    char nombre[40];
+    int ciclo;
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ISAM<Alumno, char[10]> isam("data.dat");
     return 0;
 }
